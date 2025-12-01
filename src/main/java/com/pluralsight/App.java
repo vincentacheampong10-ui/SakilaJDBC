6ae371959dbcd01eb0004864a203158d2f0a45c3
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-01 14:38:15 -0500

    SELECT city FROM city

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,15 +1,28 @@
 package com.pluralsight;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila";
         String username = "root";
-        String password = "yearup24";
+        String password = "yearup";
 
         Connection connection = DriverManager.getConnection(url, username, password);
+
+        Statement statement = connection.createStatement();
+        String query = "SELECT city FROM city ";
+
+
+        ResultSet results = statement.executeQuery(query);
+
+        while (results.next()) {
+            String city = results.getString("city");
+            System.out.println(city);
+        }
+
+
+        connection.close();
     }
 }
+
